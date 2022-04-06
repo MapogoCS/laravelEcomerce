ac0424c511f3c1d8001542200775d01779a9fef9
SerRataGo <mostafa.elserry1@gmail.com>
2022-04-06 12:23:18 +0200

    add seeder admins

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +14,39 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
+    {   
+        
         // \App\Models\User::factory(10)->create();
+        Admin::create([
+            'email' => 'superadmin@mail.com',
+            'password' =>Hash::make('123456789'),
+            'city' => 'Egypt',
+            'region' => 'Cairo',
+            'name' => 'Ahmed Helmy',
+            'phone' => '01112131415',
+            'address' => '3 st cairo, cairo',  
+       ]);
+
+       //create two admin for this system 
+       Admin::create([
+        'email' => 'admin1@mail.com',
+        'password' =>Hash::make('123456789'),
+        'city' => 'Egypt',
+        'region' => 'Cairo',
+        'name' => 'mostafa Ibrahim',
+        'phone' => '011121415',
+        'address' => '3 st cairo, cairo',
+      ]);
+
+      Admin::create([
+        'email' => 'heba@mail.com',
+        'password' =>Hash::make('123456789'),
+        'city' => 'ALex',
+        'region' => 'loran',
+        'name' => 'Heba Ibrahim',
+        'phone' => '011121415',
+        'address' => '3 st cairo, cairo',
+  ]);
+
     }
 }
