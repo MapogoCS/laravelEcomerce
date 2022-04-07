a271fc9edcd307f7873000835731444a89c60cec
SerRataGo <mostafa.elserry1@gmail.com>
2022-04-07 20:45:39 +0200

    cart version problem

--- database/migrations/2022_03_12_224332_create_carts_table.php
+++ database/migrations/2022_03_12_224332_create_carts_table.php
@@ -14,7 +14,7 @@ class CreateCartsTable extends Migration
     public function up()
     {
         Schema::create('carts', function (Blueprint $table) {
-            $table->id()->primary();
+            $table->id();
             $table->timestamps();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('product_id');
