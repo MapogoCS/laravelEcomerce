822e31563f6abdb7c1e592366fd7bfe6f96dbd13
SerRataGo <mostafa.elserry1@gmail.com>
2022-04-07 10:13:06 +0200

    edit home page and edit localhost

--- resources/views/productmail.blade.php
+++ resources/views/productmail.blade.php
@@ -8,11 +8,11 @@
 </head>
 <body>
     <span>We have added a new product check this link to see the full details</span>
-    <a href="http://localhost:4200/product-details/{{$id}}">Product Details</a>
+    <a href="https://final-iti.herokuapp.com/product-details/{{$id}}">Product Details</a>
     <h1>{{$name}}</h1>
     <h2>{{$price}} EGP</h2>
     <h3>{{$description}}</h3>
     <span>if you want to unsubscribe click here</span>
-    <a href='http://localhost:4200/cancel-subscription'>Unsubscribe</a>
+    <a href='https://final-iti.herokuapp.com/cancel-subscription'>Unsubscribe</a>
 </body>
 </html>
--- resources/views/reset_password.blade.php
+++ resources/views/reset_password.blade.php
@@ -8,6 +8,6 @@
 </head>
 <body>
     <span>Click here to reset your password</span>
-    <a href="http://localhost:4200/resetPassword/{{$token}}/{{$email}}">Reset Password</a>
+    <a href="https://final-iti.herokuapp.com/resetPassword/{{$token}}/{{$email}}">Reset Password</a>
 </body>
 </html>
